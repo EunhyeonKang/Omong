8ee2afb0c913188a0cd48380a40442afa7a7930e
Dyd6913 <aorh6913@gmail.com>
2021-06-30 18:54:10 +0900

    정신나갈꺼같아! 상품등록 db연동

--- Final/src/main/java/kr/or/partner/model/dao/PartnerDao.java
+++ Final/src/main/java/kr/or/partner/model/dao/PartnerDao.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.or.member.model.vo.User;
+import kr.or.partner.model.vo.Option;
+import kr.or.partner.model.vo.Product;
+import kr.or.partner.model.vo.Package;
 
 @Repository
 public class PartnerDao {
@@ -21,4 +24,36 @@ public class PartnerDao {
 		// TODO Auto-generated method stub
 		return sqlSession.insert("partner.insertPartner" , u);
 	}
+
+	public int packageInsert(Package pa) {		
+		return sqlSession.insert("partner.packageInsert" , pa );
+	}
+
+	public int selectPacNo() {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("partner.selectPacNo");
+	}
+
+	public int productInsert(Product pro) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("partner.productInsert", pro);
+	}
+
+	public int selectProNo() {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("partner.selectProNo");
+	}
+
+	public int optionInsert(Option op) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("partner.optionInsert", op);
+	}
+
+	
+	/*
+	 * public int packageInsert(Package pa, String[] productName, String[]
+	 * optionName, String[] optionPrice) { // TODO Auto-generated method stub return
+	 * sqlSession.insert("partner.packageInsert" , pa , productName , optionName ,
+	 * optionPrice); }
+	 */
 }
--- Final/src/main/java/kr/or/partner/model/service/PartnerService.java
+++ Final/src/main/java/kr/or/partner/model/service/PartnerService.java
@@ -2,9 +2,12 @@ package kr.or.partner.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import kr.or.partner.model.vo.Package;
 
 import kr.or.member.model.vo.User;
 import kr.or.partner.model.dao.PartnerDao;
+import kr.or.partner.model.vo.Option;
+import kr.or.partner.model.vo.Product;
 
 @Service
 public class PartnerService {
@@ -12,7 +15,7 @@ public class PartnerService {
 	private PartnerDao dao;
 
 	public User selectOnePatner(User u) {
-		
+
 		return dao.selectOnePartner(u);
 	}
 
@@ -20,5 +23,54 @@ public class PartnerService {
 		// TODO Auto-generated method stub
 		return dao.insertPartner(u);
 	}
+
+	public int packageInsert(Package pa, int[] productNum, String[] productName, String[] optionName, String[] optionPrice) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<productNum.length;i++) {
+			System.out.println("프로덕트 번호" + productNum[i]);
+		}
+		for(int i=0;i<productName.length;i++) {
+			System.out.println("프로덕트 이름" + productName[i]);
+		}
+		for(int i=0;i<optionName.length;i++) {
+			System.out.println("옵션이름 " +optionName[i]);
+		}
+		for(int i=0;i<optionPrice.length;i++) {
+			System.out.println("옵션 가격" + optionPrice[i]);	
+		}
+		int result = dao.packageInsert(pa);
+		if (result > 0) {
+
+			int pacNo = dao.selectPacNo();
+			for (int i = 0; i < productName.length; i++) {
+				Product pro = new Product();
+				pro.setPackageProductNo(pacNo);
+				pro.setProductName(productName[i]);
+				int productResult = dao.productInsert(pro);
+
+				if (productResult > 0) {
+					int proNo = dao.selectProNo();
+					for (int j = 0; j < productNum.length; j++) {
+						if (productNum[j] == i) {
+							Option op = new Option();
+							op.setProductNo(proNo);
+							op.setOptionName(optionName[j]);
+							op.setOptionPrice(optionPrice[j]);
+							int optionResult = dao.optionInsert(op);
+						}
+					}
+					
+				}else {
+					return 0;
+				}
+
+			}
+			
+		} else {
+			return 0;
+		}
+		return 1;	
+	}
 	
+
 }
